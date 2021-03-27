132fcfb682c77d43e835dfdcce751ef97c3d93c7
Daniel Scherf <munita.gavai@gmail.com>
2021-03-27 14:08:46 +0100

    Add automatic version replacement

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.apache.tools.ant.filters.ReplaceTokens
+
 plugins {
     java
 }
@@ -12,4 +14,13 @@ repositories {
 
 dependencies {
     implementation("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
+}
+
+tasks {
+    processResources {
+        from(sourceSets["main"].resources) {
+            val tokens = mapOf("version" to version)
+            filter(ReplaceTokens::class, mapOf("tokens" to tokens))
+        }
+    }
 }
